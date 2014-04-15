93dab14940228df361169d37af58d18f1d34d284
Frank Michael Sanchez <orion1701@me.com>
2014-04-15 06:37:56 -0400

    Region Search

--- Pinball/RegionsView.m
+++ Pinball/RegionsView.m
@@ -8,8 +8,10 @@
 
 #import "RegionsView.h"
 
-@interface RegionsView () {
+@interface RegionsView () <UISearchBarDelegate> {
     NSMutableArray *allRegions;
+    BOOL isSearching;
+    NSMutableArray *searchResults;
 }
 
 @end
@@ -32,29 +34,65 @@
         [allRegions addObjectsFromArray:regions];
         [self.tableView reloadData];
     }];
+    searchResults = [NSMutableArray new];
 }
 - (void)didReceiveMemoryWarning{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+#pragma mark - Searchbar Delegate
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar{
+    isSearching = YES;
+    searchBar.showsCancelButton = YES;
+}
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText{
+    NSPredicate *searchPred = [NSPredicate predicateWithFormat:@"formalName CONTAINS[cd] %@",searchText];
+    [searchResults removeAllObjects];
+    searchResults = nil;
+    searchResults = [NSMutableArray new];
+    [searchResults addObjectsFromArray:[allRegions filteredArrayUsingPredicate:searchPred]];
+    [self.tableView reloadData];
+}
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar{
+    isSearching = NO;
+    searchBar.text = @"";
+    [searchBar resignFirstResponder];
+    searchBar.showsCancelButton = NO;
+    [self.tableView reloadData];
 }
 #pragma mark - Table view data source
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     return 1;
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return allRegions.count;
+    if (!isSearching){
+        return allRegions.count;
+    }else{
+        return searchResults.count;
+    }
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"RegionCell" forIndexPath:indexPath];
     
-    NSDictionary *region = allRegions[indexPath.row];
+    NSDictionary *region;
+    if (!isSearching){
+        region = allRegions[indexPath.row];
+    }else{
+        region = searchResults[indexPath.row];
+    }
     cell.textLabel.text = region[@"formalName"];
     
     return cell;
 }
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    [[PinballManager sharedInstance] changeToRegion:allRegions[indexPath.row]];
+    NSDictionary *region;
+    if (!isSearching){
+        region = allRegions[indexPath.row];
+    }else{
+        region = searchResults[indexPath.row];
+    }
+    [[PinballManager sharedInstance] changeToRegion:region];
     [self.navigationController popToRootViewControllerAnimated:YES];
 }
 /*
