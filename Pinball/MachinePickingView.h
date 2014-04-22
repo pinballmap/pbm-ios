b0dc94f194887fed8c9bcbe62198c6d168428e8d
Frank Michael Sanchez <orion1701@me.com>
2014-04-22 16:46:11 -0400

    Add support for suggesting a new location.
    Ability to suggest a new location. Added the ability to pick machines
    that belong to that location as well.

---
+++ Pinball/MachinePickingView.h
@@ -0,0 +1,26 @@
+//
+//  MachinePickingView.h
+//  Pinball
+//
+//  Created by Frank Michael on 4/22/14.
+//  Copyright (c) 2014 Frank Michael Sanchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol PickingDelegate;
+
+@interface MachinePickingView : UITableViewController
+
+@property (nonatomic,assign) id delegate; // PickingDelegate
+
+@property (nonatomic,assign) NSArray *pickedMachines;
+
+@end
+
+
+@protocol PickingDelegate <NSObject>
+
+- (void)pickedMachines:(NSArray *)machines;
+
+@end
---
+++ Pinball/NewLocationsView.h
@@ -0,0 +1,13 @@
+//
+//  NewLocationsView.h
+//  Pinball
+//
+//  Created by Frank Michael on 4/22/14.
+//  Copyright (c) 2014 Frank Michael Sanchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewLocationsView : UITableViewController
+
+@end
