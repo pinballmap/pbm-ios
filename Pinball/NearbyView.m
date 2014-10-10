cbdbfa8190bf74e5df535392bb3de0f1390792d0
Frank Michael Sanchez <orion1701@me.com>
2014-10-10 11:29:07 -0400

    Nearby Improvements

--- Pinball/NearbyView.m
+++ Pinball/NearbyView.m
@@ -12,9 +12,10 @@
 #import "Location+UpdateDistance.h"
 #import "Location+Annotation.h"
 
-@interface NearbyView () <MKMapViewDelegate>
+@interface NearbyView () <MKMapViewDelegate,UIActionSheetDelegate>
 
 @property (weak) IBOutlet MKMapView *mapView;
+@property (nonatomic) NSNumber *filterDistance;
 
 @end
 
@@ -28,14 +29,30 @@
 - (void)viewDidAppear:(BOOL)animated{
     [super viewDidAppear:animated];
     self.mapView.showsUserLocation = YES;
+    self.filterDistance = @(10);
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateRegion) name:@"RegionUpdate" object:nil];
+    [self updateRegion];
+    
+    UIBarButtonItem *filterDistance = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"798-filter"] style:UIBarButtonItemStylePlain target:self action:@selector(updateFilter:)];
+//    self.navigationItem.leftBarButtonItem = filterDistance;
+}
+- (void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    [self.mapView removeAnnotations:self.mapView.annotations];
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (void)updateRegion{
     CLLocation *location = [[PinballMapManager sharedInstance] userLocation];
     if (location){
         [self.mapView setRegion:MKCoordinateRegionMake(location.coordinate, MKCoordinateSpanMake(.04, .04))];
         [Location updateAllForRegion:[[PinballMapManager sharedInstance] currentRegion]];
         NSFetchRequest *stackRequest = [NSFetchRequest fetchRequestWithEntityName:@"Location"];
-        stackRequest.predicate = [NSPredicate predicateWithFormat:@"region.name = %@",[[[PinballMapManager sharedInstance] currentRegion] name]];
+        stackRequest.predicate = [NSPredicate predicateWithFormat:@"region.name = %@ AND locationDistance <= %@",[[[PinballMapManager sharedInstance] currentRegion] name],self.filterDistance];
         stackRequest.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"locationDistance" ascending:YES]];
-        stackRequest.fetchLimit = 20;
+//        stackRequest.fetchLimit = 20;
         NSArray *locations = [[[CoreDataManager sharedInstance] managedObjectContext] executeFetchRequest:stackRequest error:nil];
         if (locations.count != 0){
             for (Location *location in locations) {
@@ -46,13 +63,28 @@
         }
     }
 }
-- (void)viewDidDisappear:(BOOL)animated{
-    [super viewDidDisappear:animated];
-    [self.mapView removeAnnotations:self.mapView.annotations];
+#pragma mark - Class Actions
+- (IBAction)updateFilter:(id)sender{
+    UIActionSheet *filterSheet = [[UIActionSheet alloc] initWithTitle:@"Filter Distance" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:@"10 Miles",@"20 Miles",@"30 Miles",@"40 Miles",@"50 Miles", nil];
+    [filterSheet showFromTabBar:self.tabBarController.tabBar];
 }
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Action Sheet Deleaget
+- (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex{
+    if (buttonIndex != actionSheet.cancelButtonIndex){
+        if (buttonIndex == 0){
+            self.filterDistance = @(10);
+        }else if (buttonIndex == 1){
+            self.filterDistance = @(20);
+        }else if (buttonIndex == 2){
+            self.filterDistance = @(30);
+        }else if (buttonIndex == 3){
+            self.filterDistance = @(40);
+        }else if (buttonIndex == 4){
+            self.filterDistance = @(50);
+        }
+        
+        [self updateRegion];
+    }
 }
 #pragma mark - Map Annotation
 - (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation{
