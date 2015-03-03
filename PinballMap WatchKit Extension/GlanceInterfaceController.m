0128bec60eef613940003800063e04657a1bb282
Frank Michael Sanchez <orion1701@me.com>
2015-03-03 13:54:15 -0500

    Initial Glance controller

---
+++ PinballMap WatchKit Extension/GlanceInterfaceController.m
@@ -0,0 +1,72 @@
+//
+//  GlanceInterfaceController.m
+//  PinballMap
+//
+//  Created by Frank Michael on 3/3/15.
+//  Copyright (c) 2015 Frank Michael Sanchez. All rights reserved.
+//
+
+#import "GlanceInterfaceController.h"
+
+
+@interface GlanceInterfaceController()
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *locationLabel;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *locationDistance;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *addressLabel;
+@property (weak, nonatomic) IBOutlet WKInterfaceMap *locationMap;
+
+@end
+
+
+@implementation GlanceInterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+    [WKInterfaceController openParentApplication:@{@"action":@"nearby_location"} reply:^(NSDictionary *replyInfo, NSError *error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (!error){
+                NSString *status = replyInfo[@"status"];
+                if ([status isEqualToString:@"ok"]){
+                    NSDictionary *location = replyInfo[@"body"];
+                    
+                    [self.locationLabel setText:@"Back in your head by Tegan and Sara"];//location[@"name"]];
+                    [self.addressLabel setText:[NSString stringWithFormat:@"%@, %@",location[@"street"],location[@"city"]]];
+                    
+                    if (location[@"distance"]){
+                        NSNumber *num = location[@"distance"];
+                        [self.locationDistance setText:[NSString stringWithFormat:@"%.02f miles",num.doubleValue]];
+                    }
+
+                    CLLocationCoordinate2D locationCoordinate = CLLocationCoordinate2DMake([location[@"lat"] doubleValue], [location[@"lon"] doubleValue]);
+                    [self.locationMap setRegion:MKCoordinateRegionMake(locationCoordinate, MKCoordinateSpanMake(0.01, 0.01))];
+                    [self.locationMap addAnnotation:locationCoordinate withPinColor:WKInterfaceMapPinColorRed];
+                    [self.locationMap setHidden:false];
+
+                }else{
+
+                }
+            }else{
+
+            }
+            
+        });
+    }];
+
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
