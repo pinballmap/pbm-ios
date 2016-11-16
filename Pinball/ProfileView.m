0cd6b59b7c89e2d727e4c25b19402989d8399b05
Scott Wainstock <scott.wainstock@gmail.com>
2016-11-15 20:43:50 -0800

    profile page stub

---
+++ Pinball/ProfileView.m
@@ -0,0 +1,23 @@
+#import "ProfileView.h"
+
+@interface ProfileView ()
+
+@end
+
+@implementation ProfileView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.navigationItem.title = @"Profile";
+}
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+}
+- (void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
